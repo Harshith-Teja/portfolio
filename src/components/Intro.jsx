13ac85f149809129f39e0b96f51a1ed3a737ea45
Harshith Teja <harshithtejadontoju@gmail.com>
2024-06-24 16:17:27 +0530

    updated resume link(2)

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -13,7 +13,7 @@ const Intro = () => {
         <p className='py-6 text-xl max-w-[70%] m-auto text-zinc-600 dark:text-cyan-50'>I am a dedicated individual with a strong foundation in Java, coupled with expertise in web development. My goal is to create seamless user experiences and elegant solutions driven by a constant desire for learning and innovation. I thrive in collaborative environments that allow me to contribute my technical expertise and dedication while learning from experienced developers. I&apos;m ready to contribute my skills to a dynamic team that values creativity and excellence.</p>
         
         <div className='p-6 flex max-w-[70%] m-auto justify-center items-center'>
-            <a href="https://drive.google.com/file/d/1NeT_PnIKiCZ_L71zb6_-JuYzxwbfqTd5/view?usp=drive_link" target="_blank" aria-label='resume'>
+            <a href="https://drive.google.com/file/d/1NeT_PnIKiCZ_L71zb6_-JuYzxwbfqTd5/view?usp=sharing" target="_blank" aria-label='resume'>
               <h2 className='text-xl h-20 w-32 flex items-center border-indigo-600 border-2 p-3 rounded-3xl hover:scale-110 duration-300 hover:shadow-xl hover:shadow-indigo-500 hover:bg-zinc-900 hover:text-cyan-50 dark:hover:bg-stone-300 dark:hover:text-zinc-900 transition-all ease-in-out '>View my Resume</h2>
             </a>
             <a href="#contact" aria-label='contact me'>
